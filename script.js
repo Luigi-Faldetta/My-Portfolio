3ea0ac09efb162ef100a465a5d1706caaeffc428
Luigi-Faldetta <luigifaldetta@gmail.com>
2024-06-03 13:12:56 +0200

    feat: links added

--- script.js
+++ script.js
@@ -4,6 +4,27 @@ let navbar = document.querySelector('.navbar');
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
+document.addEventListener('DOMContentLoaded', function () {
+  const bars = document.querySelectorAll('.skills .container .bar');
+  bars.forEach((bar) => {
+    bar.addEventListener('click', function () {
+      const link = bar.getAttribute('data-link');
+      if (link) {
+        window.open(link, '_blank');
+      }
+    });
+  });
+  const educationContents = document.querySelectorAll('.education .content');
+  educationContents.forEach((content) => {
+    content.addEventListener('click', function () {
+      const link = content.getAttribute('data-link');
+      if (link) {
+        window.open(link, '_blank');
+      }
+    });
+  });
+});
+
 window.onscroll = () => {
   sections.forEach((sec) => {
     let top = window.scrollY;
